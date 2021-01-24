d7eba2815a160033fafb0c2a6bafdb3eab961dd1
codyduong <duongcody507@gmail.com>
2021-01-23 23:03:41 -0600

    Update gatherer.py
    Removed some vars that was collecting way too much, instead of initing each time. Also removed some redundant checks.

--- gatherer.py
+++ gatherer.py
@@ -13,18 +13,21 @@ FIELDS = ('location', 'position', 'employer')
 
 
 class htmlParserSearch(HTMLParser):
-    allPageLinks = {} #formatted {pos: 'link', }
-    finalList = {} #formatted {'employee': [($, link),($, link),...]}
+    allPageLinks = None #formatted {pos: 'link', }
+    returnMoney = None #formatted [($, link),($, link),...]
     employee = None
 
     def __init__(self, person):
         HTMLParser.__init__(self)
+        self.allPageLinks = {}
         self.employee = person
+        self.returnMoney = []
 
     def handle_starttag(self, tag, attrs):
         links = self.allPageLinks
         if str(tag) == 'a':
-            if str(attrs[0][0]) == 'href' and str(attrs[0][1]) != '/salaries/%s/%s' % (STATE, EMPLOYER): #and not str(attrs[0][1] in ('/', '/jobs'))
+            # and not str(attrs[0][1] in ('/', '/jobs'))
+            if str(attrs[0][0]) == 'href' and str(attrs[0][1]) not in ('/salaries/%s/%s' % (STATE, EMPLOYER), '/jobs', '/') :
                 #print(attrs[0], self.getpos()[0])
                 links[(self.getpos()[0])] = (str(attrs[0][1]))
         self.allPageLinks = links
@@ -36,17 +39,9 @@ class htmlParserSearch(HTMLParser):
             if posLine in lTC:
                 moneyInt = int(str(data).strip('$').replace(',',''))
                 url = (BASE_URL + lTC[posLine])
-                try:
-                    if self.finalList[self.employee]:
-                        print('%s appended (%i, %s)' % (self.employee, moneyInt, lTC[posLine])) #the print command uses the less verbose url for the sake of me
-                        #print(self.finalList[self.employee])
-                        self.finalList[self.employee].append((moneyInt, url))
-                except:
-                    print('%s first link (%i, %s)' % (self.employee, moneyInt, lTC[posLine]))
-                    self.finalList[self.employee] = [(moneyInt, url)]
-                finally:
-                    self.allPageLinks.pop(posLine)
-                    #print(data, self.getpos()[0])
+                self.returnMoney.append((moneyInt, url))
+                print('%s appended (%i, %s)' % (self.employee, moneyInt, lTC[posLine]))
+                #print(self.allPageLinks, self.returnMoney)
 
 
 def gatherFromJSON(j):
@@ -54,10 +49,12 @@ def gatherFromJSON(j):
     Takes JSON and runs the check through govsalaries.com
     """
     decoded = json.loads(j)
+    employeeMoney = {}
     for person in decoded['employees']:
         #url = '%s/search?employer=%s&year=%i&employee=%s&state=%s' % (BASE_URL, 'olathe', 2018, urllib.parse.quote(person), 'KS')
         url = '%s/search?employer=%s&employee=%s&state=%s' % (BASE_URL, 'olathe', urllib.parse.quote(person), 'KS')
         #print(url)
         parser = htmlParserSearch(str(person))
         parser.feed(requester.runRequest(url))
+        employeeMoney[str(person)] = parser.returnMoney
         
