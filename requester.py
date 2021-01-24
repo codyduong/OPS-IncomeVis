7553c5985edde96ba4ac8ca041a74c4e671a5d69
codyduong <duongcody507@gmail.com>
2021-01-23 22:59:20 -0600

    retry script

--- requester.py
+++ requester.py
@@ -1,10 +1,22 @@
 import requests
 
+def retry(l):
+    if str(input("Try again? [Y/N]")).lower() == "y":
+        return runRequest(l)
+    elif str(input("Try again? [Y/N]")).lower() == "n":
+        return
+    else:
+        return retry(l)
+
 
 def runRequest(l):
-    r = requests.get(l)
-    if r.status_code == 200:
-        return r.text
-    else:
-        print('There was an error with accessing %s with error code %s retrieved' % (l, r.status_code))
-        return
+    try:
+        r = requests.get(l, timeout=5)
+        if r.status_code == 200:
+            return r.text
+        else:
+            print('There was an error with accessing %s with error code %s retrieved' % (l, r.status_code))
+            return retry(l)
+    except:
+        print('There was a timeout')
+        return retry(l)
